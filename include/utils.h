01f5964a54ebf96b7782a483c2de9c425418153b
Filipe Mallmann Siota <filipesiota@gmail.com>
2025-06-21 16:38:44 -0300

    feat: init project

---
+++ include/utils.h
@@ -0,0 +1,7 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+void sysCommand(char *command);
+void normalizeString(char *string);
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "utils.h"
+
+int main() {
+    
+    
+    printf("\nPrograma finalizado!\n");
+    sysCommand("pause_terminal");
+
+    return 0;
+}
